fcbedc975c0b01ed17f6ba943eda8a37060761a8
ZeyaD HessieN <zeyad.hessien1123@gmail.com>
2022-09-15 17:55:03 +0200

    Finish posts repository implementation in data layer

--- lib/features/posts/data/datasources/post_remote_data_source.dart
+++ lib/features/posts/data/datasources/post_remote_data_source.dart
@@ -2,21 +2,20 @@ import 'package:clean_architecture_course/features/posts/data/models/post_model.
 import 'package:dartz/dartz.dart';
 
 abstract class PostRemoteDataSource {
-  /// <PostModel> Not Post Because We're in Data Layer
   Future<List<PostModel>> getAllPosts();
-  Future<Unit> addPost(int postID);
-  Future<Unit> deletePosts(PostModel postModel);
+  Future<Unit> addPost(PostModel postModel);
+  Future<Unit> deletePosts(int postId);
   Future<Unit> updatePosts(PostModel postModel);
 }
 
 class PostRemoteDataSourceImpl implements PostRemoteDataSource {
   @override
-  Future<Unit> addPost(int postID) {
+  Future<Unit> addPost(PostModel postModel) {
     throw UnimplementedError();
   }
 
   @override
-  Future<Unit> deletePosts(PostModel postModel) {
+  Future<Unit> deletePosts(int postId) {
     throw UnimplementedError();
   }
 
@@ -33,49 +32,3 @@ class PostRemoteDataSourceImpl implements PostRemoteDataSource {
 
 }
 
-/// Why use abstract class Above ? OR Why don't use these one of classes of following directly
-/// Until I can implements more than one time
-/// So that if there is a problem in one of following class, I have another class to copy methods , Don't Impl From 0
-// class PostRemoteImplWithDio implements PostRemoteDataSource {
-//   @override
-//   Future<Unit> addPost() {
-//     throw UnimplementedError();
-//   }
-//
-//   @override
-//   Future<Unit> deletePosts() {
-//     throw UnimplementedError();
-//   }
-//
-//   @override
-//   Future<List<PostModel>> getAllPosts() {
-//     throw UnimplementedError();
-//   }
-//
-//   @override
-//   Future<Unit> updatePosts() {
-//     throw UnimplementedError();
-//   }
-//
-// }
-// class PostRemoteImplWithHttp implements PostRemoteDataSource {
-//   @override
-//   Future<Unit> addPost() {
-//     throw UnimplementedError();
-//   }
-//
-//   @override
-//   Future<Unit> deletePosts() {
-//     throw UnimplementedError();
-//   }
-//
-//   @override
-//   Future<List<PostModel>> getAllPosts() {
-//     throw UnimplementedError();
-//   }
-//
-//   @override
-//   Future<Unit> updatePosts() {
-//     throw UnimplementedError();
-//   }
-// }
--- lib/features/posts/domain/usecases/update_posts.dart
+++ lib/features/posts/domain/usecases/update_posts.dart
@@ -3,9 +3,9 @@ import '../../../../core/error/failure.dart';
 import '../entities/post_entity.dart';
 import '../repositories/post_repositories.dart';
 
-class GetAllPostsUseCase {
+class UpdatePostsUseCase {
   final PostRepository repositories;
-  GetAllPostsUseCase({required this.repositories});
+  UpdatePostsUseCase({required this.repositories});
   Future<Either<Failure,Unit>> call(Post post) async {
     return await repositories.updatePost(post);
   }
